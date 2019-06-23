602212f9eab39a8948d975cdec31109be3bbabbb
Juliet <chepngetichjuliet@gmail.com>
2019-06-23 15:23:50 +0300

    creates configurations for the class

--- app/views.py
+++ app/views.py
@@ -7,7 +7,17 @@ def index():
     """
     View root page function that returns the index page and its data
     """
+    message = 'Hello Readers'
 
-    return render_template('index.html')
+    return render_template('index.html',message = message)
+
+@app.route('/news/<news_id>')
+def movie(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = news_id)
+
 
     
